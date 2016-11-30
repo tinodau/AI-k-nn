09b0ad091cd14a569e0631453e6bd4e6daac41af
TinoDau <daulat.rachmanto@gmail.com>
2016-11-30 17:22:28 +0700

    committing testing - training/acuration

--- Training/tubes-ai3-K31.py
+++ Training/tubes-ai3-K31.py
@@ -2,28 +2,21 @@ import csv
 import random
 import math
 import operator
-import copy
+
 
 # GLOBAL VARIABLE
 trainingSet = [] # set of training data
 testSet     = [] # set of testing data
 prediction  = [] # list of result prediction
-
-dataTestSet = []
-testAnswer  = []
-
-jawaban     = []
-
-k           = 131  # TOTAL OF NEIGHBORS (best 13 to 21)
-trainingFile= 'Train.csv' # cobatraining.csv
-testingFile = 'Test.csv'
-split       = 1 # BEST 65
+k           = 31  # TOTAL OF NEIGHBORS (best 13 to 21)
+filename    = 'Train.csv' # cobatraining.csv
+split       = 0.90 # BEST 65
 
 
-def readTrain(training, test):
+def readData(training, test):
     """Load Data from given file"""
 
-    with open (trainingFile, 'rb') as csvfile:
+    with open (filename, 'rb') as csvfile:
         datalist    = csv.reader(csvfile)   #load data from file
         next(datalist, None)
         dataset     = list(datalist)        #put data to var dataset
@@ -43,30 +36,11 @@ def readTrain(training, test):
                 test.append(inp)
 
         print 'Trainingset    = ' + str(len(training))
+        print 'Testset        = ' + str(len(test)) + '\n'
 
         # print ('data = {0} \n').format(test)
 
-def readTest(dataTestSet) :
-    """ Load Data from testfile """
-    with open (testingFile, 'rb') as csvfile:
-        dataTestList    = csv.reader(csvfile)
-        next(dataTestList, None)
-        dataSetTest     = list(dataTestList)
-        for y in range(len(dataSetTest)) :
-            inp = []
 
-            # print dataSetTest[y]
-            for x in range(1, 11) :
-                inp.append(float(dataSetTest[y][x]))
-
-            # if random.random() > 0 :
-            dataTestSet.append(inp)
-
-            # print dataTestSet[y]
-    # print dataTestSet
-
-    print 'Testset        = ' + str(len(dataTestSet)) + '\n'
-    return dataTestSet
 
 def normalize(training, test):
     """ Normalize data to [0,1] only (Binary classification) """
@@ -121,7 +95,8 @@ def voting(ranking):
     """ RETURN MOST VOTED - FOR RESULT """
 
     majorvote    = {}
-    # PUT test
+    # PUT MOJORITY VOTED HERE
+
     for y in range(len(ranking)) :
     # RANK FROM 1 TO 3
         vote    = ranking[y][-1]
@@ -135,71 +110,45 @@ def voting(ranking):
 
     sortingvote = sorted(majorvote.iteritems(), key=operator.itemgetter(1), reverse=True)
     # SORTING MOST VOTED
+
     return sortingvote[0][0]
 
 
-# def getResult(training, test, prediction, k):
-#     """ RETURN LIST F RESULT """
-#
-#     for i in range(len(test)) :
-#         ranking = getRanking(training, test[i], k)
-#         vote  = voting(ranking)
-#
-#         prediction.append(vote)
-#         print str(testSet[i]) + ' ==> ' + str(prediction[i])
+def getResult(training, test, prediction, k):
+    """ RETURN LIST F RESULT """
 
-    # print # ENTERING SPACE
+    for i in range(len(test)) :
+        ranking = getRanking(training, test[i], k)
+        vote  = voting(ranking)
 
-def getResultTest (trainingnya, dataTestSet,  testAnswer, k) :
-    trainingFilenya = readTrain(trainingSet, testSet)
-    testFilenya = readTest(dataTestSet)
+        prediction.append(vote)
+        print str(testSet[i]) + ' ==> ' + str(prediction[i])
 
-    # print testFilenya
-    # testAnswer.insert(0, "START TESTING")
-    for i in range(len(testFilenya)) :
-        ranking = getRanking(trainingnya,  testFilenya[i], k)
-        vote    = voting(ranking)
-        print(i, testFilenya[i])
-        testAnswer.append(vote)
-
-    tmp = copy.copy(testFilenya)
-
-    for jawab in range(len(testFilenya)) :
-        tmp[jawab].append(testAnswer[jawab])
-        jawaban.append(tmp[jawab])
-
-    with open("K-" + str(k) +".csv", "wb") as startTest:
-        writer = csv.writer(startTest)
-        writer.writerow(["x1", "x2", "x3" , "x4", "x5", "x6", "x7", "x8", "x9", "x10", "y"])
-        for row in range(len(testFilenya)) :
-            writer.writerow(jawaban[row])
-        startTest.close()
+    print # ENTERING SPACE
 
 def accuration(test, prediction):
     """ RETURN PERSENTAGE OF ACCURATION """
 
     correct = 0     # SET TOTAL OF CORRECT FROM 0
+
     for y in range(len(test)) :
     # LISTING LINES
         if test[y][-1] == prediction[y]:
             correct += 1
 
+
     # print ('data = {0}').format(prediction)
     print 'Accuration = '+ str((correct / float (len(test))) * 100.0) + '%'
+
     # print correct
     # RETURN PERSENTAGE OF CORRECT RESULT
 
 
 def main():
     """ MAIN FUNCTION """
-    # readTrain(trainingSet, testSet)                  # READ DATA
-    # getResult(trainingSet, testSet, prediction, k)  # GET RESULT
+    readData(trainingSet, testSet)                  # READ DATA
+    getResult(trainingSet, testSet, prediction, k)  # GET RESULT
+    accuration(testSet, prediction)                 # GET ACCURATION
 
-    # readTest(dataTestSet)
-    # print readTest(dataTestSet)
-    dataTraining = []
-    getResultTest(trainingSet, dataTestSet, testAnswer, k)
-
-    # accuration(testSet, prediction)                 # GET ACCURATION
 
 main()
